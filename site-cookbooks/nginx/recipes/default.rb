1483c6a028c5fe26bc1805ff947862c0b1b4efd9
Mizuki Yamanaka <charles.vineyard@gmail.com>
2013-11-17 23:19:06 +0900

    Add nginx service.

--- site-cookbooks/nginx/recipes/default.rb
+++ site-cookbooks/nginx/recipes/default.rb
@@ -12,3 +12,9 @@ include_recipe "yum::epel"
 package "nginx" do
   action :install
 end
+
+service "nginx" do
+  supports :status => true, :restart => true, :reload => true
+  action [ :enable, :start ]
+end
+
