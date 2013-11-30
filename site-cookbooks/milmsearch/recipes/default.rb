da195cc3d8e911f9b19b2c5f42f4f00297e7a4a5
Mizuki Yamanaka <charles.vineyard@gmail.com>
2013-12-01 02:32:40 +0900

    Fix user params and Create supervisor_service

--- site-cookbooks/milmsearch/recipes/default.rb
+++ site-cookbooks/milmsearch/recipes/default.rb
@@ -7,11 +7,34 @@
 # All rights reserved - Do Not Redistribute
 #
 
+# Create deploy user
+data_ids = data_bag('users')
+data_ids.each do |id|
+  u = data_bag_item('users', id)
+  user u['username'] do
+    home  u['home']
+    group u['group']
+    shell u['shell']
+    password nil
+    supports :manage_home => true
+  end
+
+  directory "#{u['home']}/.ssh" do
+    owner u['username']
+    group u['group']
+    mode 0700
+    action :create
+    only_if "test -d #{u['home']}"
+  end
+
+  file "#{u['home']}/.ssh/authorized_keys" do
+    owner u['username']
+    group u['group']
+    mode 0600
+    action :create
+    content u['key']
+  end  
+end
+
 include_recipe "milmsearch::supervisor"
 
-user "milmsearch" do
-  home "/usr/local/milmsearch"
-  shell "/bin/bash"
-  password nil
-  supports :manage_home => true
-end
--- site-cookbooks/milmsearch/recipes/supervisor.rb
+++ site-cookbooks/milmsearch/recipes/supervisor.rb
@@ -9,6 +9,7 @@
 
 include_recipe "supervisor"
 
+# まだopscodeのレシピでCentOS対応してないので自動起動だけここでする 
 ruby_block "run_supervisord_on_boot" do
   block do
     file = Chef::Util::FileEdit.new("/etc/rc.local")
@@ -22,4 +23,12 @@ execute "start_supervisord" do
   cwd "/tmp"
   command "/usr/bin/supervisord"
   creates "/var/run/supervisord.pid"
+end
+
+supervisor_service "milmsearch" do
+  command "/usr/local/milmsearch/target/start -Dconfig.file=/usr/local/milmsearch/conf/application-production.conf -DapplyEvolutions.default=true"
+  process_name "milmsearch"
+  user "milmsearch"
+  stdout_logfile "/var/log/milmsearch.log"
+  stderr_logfile "/var/log/milmsearch-err.log"
 end
