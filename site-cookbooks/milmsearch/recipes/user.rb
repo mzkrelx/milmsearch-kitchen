c83d4ef7982618ba620c33d34bc7f328c517863d
Kato Ryoko <k_ryoko@geishatokyo.com>
2014-02-16 13:53:17 +0900

    add settings

--- site-cookbooks/milmsearch/recipes/user.rb
+++ site-cookbooks/milmsearch/recipes/user.rb
@@ -39,3 +39,20 @@ file "#{u['home']}/.ssh/authorized_keys" do
   action :create
   content u['key']
 end  
+
+# no sudo password for user
+# lines = { 'export PYENV_ROOT="\$HOME\/.pyenv"' => 'export PYENV_ROOT="$HOME/.pyenv"',
+#   'export PATH="\$PYENV_ROOT\/bin:\$PATH"' => 'export PATH="$PYENV_ROOT/bin:$PATH"',
+#   'eval "\$\(pyenv init -\)"' => 'eval "$(pyenv init -)"'}
+# 
+# file = Chef::Util::FileEdit.new("#{me['home']}/.bashrc")
+# lines.each { |reg, line|
+#   file.insert_line_if_no_match(/#{reg}/, line)
+#   file.write_file
+# }
+
+file = Chef::Util::FileEdit.new("/etc/sudoers/")
+file.insert_line_if_no_match(
+  /%#{u['username']} ALL=\(ALL\) NOPASSWD: \/usr\/bin\/supervisorctl/, 
+  "%#{u['username']} ALL=(ALL) NOPASSWD: /usr/bin/supervisorctl")
+file.write_file
